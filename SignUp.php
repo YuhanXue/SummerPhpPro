4900b97e245206c06ab5590ca80a6e1fc3feb67d
Yuhan Xue <35746251+YuhanXue@users.noreply.github.com>
2018-05-12 02:12:50 +0800

    Update

--- SignUp.php
+++ SignUp.php
@@ -42,10 +42,37 @@
 	        else
 		        $error[] = "Please enter your email";
 
+	        $user_degree=$_POST["user_degree"];
 	        if(!empty($_POST['user_degree']))
-		        $user_degree=$_POST["user_degree"];
+	        {
+		        if($user_degree == 'master')
+			        echo "Master";
+		        else if($user_degree == "bachelor")
+			        echo "Bachelor";
+		        else if($user_degree == "null")
+                    echo "Null";
+	        }
 	        else
 		        $error[] = "Please select your degree";
+
+	        if (!empty($error)){
+		        foreach ($error as $msg)
+		        {
+			        echo $msg;
+			        echo '<br>';
+		        }
+	        }
+	        else{
+		        // Create connection
+		        include("includes/db_connection.php");
+		        $conn = new mysqli($servername, $username, $password, $db);
+            }
+
+            //check connection
+            if($conn->connect_error){
+	            die("Connection failed: " . $conn->connect_error);
+            }
+
         }
     ?>
 
@@ -54,55 +81,61 @@
         <img src="" class="user">
         <h1 class=""> Sign Up</h1>
 
-        <form class="" id="Form">
+        <form class="" id="Form" method="post">
             <div id="username">
                 <p>Username</p>
                 <div id="box1">
-                    <input type="text" id="Username" name="user_name" placeholder="Create a Username">
+                    <input type="text" id="Username" name="user_name" placeholder="Create a Username"
+                    value=<?php if (isset($_POST['user_name'])) echo $_POST['user_name'] ?>>
                 </div>
             </div>
 
             <div id="password">
                 <p>Password</p>
                 <div id="box2">
-                    <input type="password" id="password" name="user_password" placeholder="Create a Password" >
+                    <input type="password" id="password" name="user_password" placeholder="Create a Password"
+                    value=<?php if(isset($_POST['user_password'])) echo $_POST['user_password']?>>
                 </div>
             </div>
 
             <div id="idnumber">
                 <p>ID Number</p>
                 <div id="box3">
-                    <input type="text" id="id_number" name="user_id" placeholder="Please enter your ID number" >
+                    <input type="text" id="id_number" name="user_id" placeholder="Please enter your ID number"
+                           value=<?php if(isset($_POST['user_id'])) echo $_POST['user_id']?>>
                 </div>
             </div>
 
             <div id="college">
                 <p>College</p>
                 <div id="box4">
-                    <input type="text" id="college" placeholder="Please enter your college" name="user_college">
+                    <input type="text" id="college" placeholder="Please enter your college" name="user_college"
+                           value=<?php if(isset($_POST['user_college'])) echo $_POST['user_college']?>>
                 </div>
             </div>
 
             <div id="email">
                 <p>Email</p>
                 <div id="box5">
-                    <input type="email" id="email" placeholder="Please enter your Email" name="user_email">
+                    <input type="email" id="email" placeholder="Please enter your Email" name="user_email"
+                           value=<?php if(isset($_POST['user_email'])) echo $_POST['user_email']?>>
                 </div>
             </div>
 
             <div id="degree">
                 <p>Academic Degree</p>
                 <div class="select">
-                    <select name="user_degree" id="degree">
+                    <select name="user_degree" id="degree" size="1">
+                        <option value="null">Null</option>
                         <option value="bachelor">Bachelor</option>
                         <option value="master">Master</option>
                     </select>
                 </div>
-            </div>
 
             <div id="submit">
                 <input type="submit" name="" value="Register">
             </div>
+
         </form>
     </div>
 </body>
